5c7aabafa9cc025ab3b1273e1d81fa5e32c65995
Max Silbiger <max.silbiger@gmail.com>
2015-10-06 00:50:51 -0400

    script removes e67f characters now

--- assisted_dump.py
+++ assisted_dump.py
@@ -4,14 +4,38 @@
 # Open opening in write mode.
 # Clean opening, removing large quantities of leading spaces and adjusting the offeset based on how many spaces are removed.
 # Insert the text into the excel spreadsheet. Columns: File, Offset, Japanese, English.
-
 import subprocess
+import codecs
+import xlsxwriter
 
-subprocess.call(".\SJIS_Dump OPENING.EXE opening 4 0")
+subprocess.call(".\SJIS_Dump OPENING.EXE opening 7 0")
 
+# need to remove characters like E67F that freak the SJIS parser out for some reason
+clean_bytes_string = ""
+
+with open('opening', 'rb') as f:
+    byte = f.read(2)
+    while byte !="":
+        if byte.encode('hex') != "e67f":
+            clean_bytes_string += byte
+        else:
+            print "Found an E67F, got rid of it"
+        print byte.encode('hex')
+        byte = f.read(2)
+
+#clean_bytes_string.replace("\xe6\x7f", "")
+print clean_bytes_string
+
+clean_bytes = bytearray(clean_bytes_string)
+
+with open('opening', 'wb') as f:
+    f.write(clean_bytes)
+        
+  
+        
 dump = {}
 
-fo = open("opening", "r+")
+fo = codecs.open("opening", "r", encoding='shift_jis')
 lines = fo.readlines()
 
 for n in range(0, len(lines)+1, 3):
@@ -20,14 +44,14 @@ for n in range(0, len(lines)+1, 3):
     #print offset_string
     #print offset
     text_raw = lines[n+1]
-    print text_raw[0:2]
-    while text_raw[0:2] == '\xc3':    # checking for the space.  (Encoding error)
-        text_raw = text_raw[5:]
-        offset += 2
+    #print text_raw[0:2]             # omg this was breaking it???
+    #while text_raw[0:2] == '\xc3':    # checking for the space.  (Encoding error)
+    #    text_raw = text_raw[5:]
+    #    offset += 2
     hex_offset = hex(offset)
     
     print hex_offset
-    print text_raw
+    #print text_raw
     
     dump[hex_offset] = text_raw
     
