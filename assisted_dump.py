e172bddbe9e6d2518f993d839c786a96ad653f05
Max Silbiger <max.silbiger@gmail.com>
2015-10-08 15:20:14 -0400

    Now dumps a list of files

--- assisted_dump.py
+++ assisted_dump.py
@@ -9,55 +9,62 @@ import subprocess
 import codecs
 import xlsxwriter
 
-subprocess.call(".\SJIS_Dump OPENING.EXE opening 7 0")
+files = ['OPENING.EXE',]
 
-# need to remove characters like E67F that freak the SJIS parser out for some reason
-clean_bytes_string = ""
+for file in files:
+    file_dump = file + "_dump"
+    subprocess.call(".\SJIS_Dump %s %s 7 0" % (file, file_dump))
 
-with open('opening', 'rb') as f:
-    byte = f.read(2)
-    while byte !="":
-        if byte.encode('hex') != "e67f":
-            clean_bytes_string += byte
-        else:
-            print "Found an E67F, got rid of it"
-        #print byte.encode('hex')
+    # need to remove characters like E67F that freak the SJIS parser out for some reason
+    clean_bytes_string = ""
+
+    with open(file_dump, 'rb') as f:
         byte = f.read(2)
+        while byte !="":
+            if byte.encode('hex') != "e67f":
+                clean_bytes_string += byte
+            else:
+                print "Found an E67F, got rid of it"
+            #print byte.encode('hex')
+            byte = f.read(2)
 
-#print clean_bytes_string
+    #print clean_bytes_string
 
-clean_bytes = bytearray(clean_bytes_string)
+    clean_bytes = bytearray(clean_bytes_string)
 
-with open('opening', 'wb') as f:
-    f.write(clean_bytes)
+    with open(file_dump, 'wb') as f:
+        f.write(clean_bytes)
     
-# Now the SJIS-Dump is clean, parse it and deal with it in memory.
+    # Now the SJIS-Dump is clean, parse it and deal with it in memory.
         
-dump = {}
+    dump = {}
 
-fo = codecs.open("opening", "r", encoding='shift_jis')
-lines = fo.readlines()
+    fo = codecs.open(file_dump, "r", encoding='shift_jis')
+    lines = fo.readlines()
 
-for n in range(0, len(lines)-1, 3):
-    offset_string = lines[n][11:].rstrip()     # first line of three, minus "Position : ", minus "\n"
-    offset = hex(int(offset_string, 16))
+    for n in range(0, len(lines)-1, 3):
+        offset_string = lines[n][11:].rstrip()     # first line of three, minus "Position : ", minus "\n"
+        offset = hex(int(offset_string, 16))
     
-    text = lines[n+1]
+        text = lines[n+1]
     
-    dump[offset] = text
+        dump[(file, offset)] = text
     
     
-workbook = xlsxwriter.Workbook('opening_dump.xlsx')
-worksheet = workbook.add_worksheet()
+    workbook = xlsxwriter.Workbook('opening_dump.xlsx')
+    worksheet = workbook.add_worksheet()
 
-worksheet.set_column('C:C', 75)
+    worksheet.set_column('A:A', 30)
+    worksheet.set_column('C:C', 80)
+    worksheet.set_column('D:D', 90)
 
-row = 0
+    row = 0
 
-for source, text in dump.iteritems():
-    # excel cols: File, Offset, Japanese, English
-    worksheet.write(row, 1, offset)
-    worksheet.write(row, 2, text)
-    row += 1
+    for source, text in dump.iteritems():
+        # excel cols: File, Offset, Japanese, English
+        worksheet.write(row, 0, source[0])
+        worksheet.write(row, 1, source[1])
+        worksheet.write(row, 2, text)
+        row += 1
     
-workbook.close()
+    workbook.close()
