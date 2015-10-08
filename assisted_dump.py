e7ced105ab57b62bdd59c0c634cffbb134edcfa9
Max Silbiger <max.silbiger@gmail.com>
2015-10-07 23:13:07 -0400

    Fix some docs, remove some debug stuff

--- assisted_dump.py
+++ assisted_dump.py
@@ -1,9 +1,10 @@
 # Makes use of a tool called SJIS_Dump which deals with the encoding better.
 
 # Execute the command ".\SJIS_Dump OPENING.EXE opening 4 0"
-# Open opening in write mode.
-# Clean opening, removing large quantities of leading spaces and adjusting the offeset based on how many spaces are removed.
+# Clean that dump of any characters that freak out my SJIS decoder.
+# Parse the dump into offset:text pairs.
 # Insert the text into the excel spreadsheet. Columns: File, Offset, Japanese, English.
+
 import subprocess
 import codecs
 import xlsxwriter
@@ -20,10 +21,10 @@ with open('opening', 'rb') as f:
             clean_bytes_string += byte
         else:
             print "Found an E67F, got rid of it"
-        print byte.encode('hex')
+        #print byte.encode('hex')
         byte = f.read(2)
 
-print clean_bytes_string
+#print clean_bytes_string
 
 clean_bytes = bytearray(clean_bytes_string)
 
@@ -40,20 +41,8 @@ lines = fo.readlines()
 for n in range(0, len(lines)-1, 3):
     offset_string = lines[n][11:].rstrip()     # first line of three, minus "Position : ", minus "\n"
     offset = hex(int(offset_string, 16))
-    #offset = hex(offset_string)
-    print offset
-    #print offset_string
-    #print offset
-    text = lines[n+1]
-    # no longer going to get rid of the spaces... it'll help to know how much room we have.
-    #print text_raw[0:2]
-    #while text_raw[0:2] == '\xc3':    # checking for the space.  (Encoding error)
-    #    text_raw = text_raw[5:]
-    #    offset += 2
-    #hex_offset = hex(offset)
     
-    #print hex_offset
-    #print text_raw
+    text = lines[n+1]
     
     dump[offset] = text
     
@@ -61,7 +50,7 @@ for n in range(0, len(lines)-1, 3):
 workbook = xlsxwriter.Workbook('opening_dump.xlsx')
 worksheet = workbook.add_worksheet()
 
-worksheet.set_column('C:C', 50)
+worksheet.set_column('C:C', 75)
 
 row = 0
 
@@ -69,8 +58,6 @@ for source, text in dump.iteritems():
     # excel cols: File, Offset, Japanese, English
     worksheet.write(row, 1, offset)
     worksheet.write(row, 2, text)
-    #print text
     row += 1
-    
     
 workbook.close()
