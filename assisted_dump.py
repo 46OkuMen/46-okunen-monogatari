06af6fb241889ce37c754a80c3e1cbce16b2b396
Max Silbiger <max.silbiger@gmail.com>
2015-10-24 23:34:46 -0400

    Fix absurd duplications of everything, fix inaccurate offset calculation of later strings in a snippet

--- assisted_dump.py
+++ assisted_dump.py
@@ -4,35 +4,14 @@
 # Parse the dump into (source, offset, dump).
 # Insert the text into the excel spreadsheet. Columns: File, Offset, Japanese, English.
 
-# TODO: SJIS-Dump might not be the best tool for the job - like in ST1.EXE:0xfb00, it gets off-by-one
-# and reads some pretty innocuous hiragana as really dense meaningless kanji. 
-# (Like, it reads 82-E9 82-E6 as ...E9-82 E6-82.)
-# This is really hard to recognize and will result in a bad translation.
-# I should look into writing a script to do this myself, or at least see if I can modify SJIS-Dump.
+# SJIS_Dump on its own messes up any file larger than 0x100 bytes, since its internal buffer is
+# that size. It splits two-byte characters across multiple buffers, and as a result, it mis-encodes them.
+# Trying to alter the C++ program to have a larger buffer had some odd results, so my solution:
+# Split up the source files themselves into 0x100 and smaller chunks by splitting them into the
+# game text lines themselves, which are never longer than the game window.
 
-# Errors:
-# ST1.EXE, 0xdefd-dfd00
-# ST1.EXE, 0xfb00
-# ST1.EXE, 0xe000-e001
-# ST1.EXE, 0x100ff-10100
-# ST1.EXE, 0x10e00
-# ST1.EXE, 0x113df-113e0
-
-# ST2.EXE, 0xc900
-#            d700
-#            d901
-#            da00
-#            db00
-#            dc00
-
-# ST3.EXE, 0xbb00
-
-# ST4.EXE, 0x120b4 (just a random piece of kanji, fix with the ranges)
-
-# So these errors only really show up at 00, sometimes at 0. Why???
-# Answer: SJIS_Dump has a buffer 0x100 long. So it'll always misread the 0x100th byte whenever there's carryover.
-# I'll just modify the source code to have a large enough buffer.
-# That seems to have weird effects - when it's too large, it just dumps the same few sequences a lot... weird.
+# TODO: Make sure the offsets are correct. (Especially for the different strings in one snippet.)
+# TODO: Look for garbage.
 
 # TODO: Add a column for the string's pointer location, if there is one.
 # TODO: Add control codes?
@@ -41,11 +20,17 @@ import os
 import subprocess
 import codecs
 import xlsxwriter
+
+# Game contains disks.
+# Disks contain files.
+# Files contain blocks.
+# Blocks contain snippets, separated by \x00. (<END> tags)
+# Snippets contain strings, separated by \x0a or \x0d. (line beaks)
         
 
 # Dict of files and dump blocks.
 files = [('ST1.EXE', ((0xd873, 0xd933), (0xd984, 0x10f85), (0x10fca, 0x11565), (0x11839, 0x119a3), (0x11d42, 0x1204e),),),
-          ('ST1.EXE', ((0xd873, 0x117df), (0x11838, 0x119a3), (0x11d42, 0x1240d),),),
+#          ('ST1.EXE', ((0xd873, 0x117df), (0x11838, 0x119a3), (0x11d42, 0x1240d),),),
 #          ('ST2.EXE', ((0xc23b, 0x1085e),),),
 #          ('ST3.EXE', ((0xb49d, 0xee70),),),
 #          ('ST4.EXE', ((0xe263, 0x1620d), (0x1659c, 0x168a8)),),
@@ -74,46 +59,40 @@ dump_files = []
 
 for file in files:
     for (block_start, block_end) in file[1]:
-        print file[0]
         block_length = block_end - block_start
-        print block_length
         in_file = open(file[0], 'rb')
         in_file.seek(block_start)
         bytes = in_file.read(block_length)
         only_hex = ""
         for c in bytes:
-            #print ord(c)
             only_hex += "\\x%02x" % ord(c)
-        #print only_hex
         snippets = only_hex.split('\\x00')
         for snippet in snippets:
             if snippet and len(snippet) > 4:
                 snippet_start = only_hex.index(snippet)
-                #print snippet
                 offset = hex(block_start + (snippet_start / 4))
-                #print offset
                 snippet_filename = "snippet_" + offset + "_" + file[0]
                 snippet_file = open(snippet_filename, 'wb')
                 snippet_bytes = snippet.replace('\\x', '').decode('hex')
                 snippet_file.write(snippet_bytes)
                 snippet_file.close()
                 
+                
                 snippet_dump = "dump_" + snippet_filename
-                #print snippet_dump
                 subprocess.call(".\SJIS_Dump %s %s 1 0" % (snippet_filename, snippet_dump))
                 
                 dump_files.append(snippet_dump)
-#print dump_files
+                os.remove(snippet_filename)
         
 dump = []
+print dump_files
+print len(dump_files)
         
 for file in dump_files:
     # file.split('_') is ('dump', 'snippet', sourcefile, offset)
     source = file.split('_')[3]
     offset = file.split('_')[2]
-    # SJIS-Dump spits out some garbage characters not included in SJIS specification.
-    # Rather than try to get rid of them, just ignore all decoding errors and it'll be fine.
-    # Also, sticking to the ranges that have been mapped out will reduce the noise.
+
     fo = codecs.open(file, "r", encoding='shift_jis', errors='ignore') 
 
     lines = fo.readlines()
@@ -121,25 +100,31 @@ for file in dump_files:
     for n in range(0, len(lines)-1, 3):
         offset_string = lines[n][11:].rstrip()     # first line of three, minus "Position : ", minus "\n"
         offset_within_snippet = hex(int(offset_string, 16))
-        print offset
-        print offset_within_snippet
+        
         try:
-            offset = int(offset, 16) + int(offset_within_snippet, 16)
+            total_offset = int(offset, 16)
         except TypeError:
-            offset += int(offset_within_snippet, 16)
-    
+            total_offset = offset
+            
+        total_offset += int(offset_within_snippet, 16)
+            
+        total_offset = hex(total_offset)
+        print total_offset
+        
         text = lines[n+1]
     
-        dump.append((source, offset, text))
-
-    for snippet in dump:
-        # excel cols: File, Offset, Japanese, English
-        worksheet.write(excel_row, 0, snippet[0])
-        worksheet.write(excel_row, 1, snippet[1])
-        worksheet.write(excel_row, 2, snippet[2])
-        excel_row += 1
-    
+        dump.append((source, total_offset, text))
+        
     fo.close()
+
+# Access this in a separate for loop, since there might be multiple texts in a snippet
+for snippet in dump:
+    # excel cols: File, Offset, Japanese, English
+    worksheet.write(excel_row, 0, snippet[0])
+    worksheet.write(excel_row, 1, snippet[1])
+    worksheet.write(excel_row, 2, snippet[2])
+    excel_row += 1
+
     
     #os.remove(file_dump)
 
