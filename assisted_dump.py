a1e646bfff033df8fa483091fa5879863b10b5a3
Max Silbiger <max.silbiger@gmail.com>
2015-10-26 00:22:56 -0400

    Fix .dat delimiters

--- assisted_dump.py
+++ assisted_dump.py
@@ -10,8 +10,10 @@
 # Split up the source files themselves into 0x100 and smaller chunks by splitting them into the
 # game text lines themselves, which are never longer than the game window.
 
-# TODO: Make sure the offsets are correct. (Especially for the different strings in one snippet.)
 # TODO: Look for garbage.
+# TODO: Where is the weird ASCII at the end of SINKA.DAT strings coming from?
+
+# TODO: Print progress.
 
 # TODO: Add a column for the string's pointer location, if there is one.
 # TODO: Add control codes?
@@ -29,20 +31,20 @@ import xlsxwriter
         
 
 # Dict of files and dump blocks.
-files = [('ST1.EXE', ((0xd873, 0xd933), (0xd984, 0x10f85), (0x10fca, 0x11565), (0x11839, 0x119a3), (0x11d42, 0x1204e),),),
-#          ('ST1.EXE', ((0xd873, 0x117df), (0x11838, 0x119a3), (0x11d42, 0x1240d),),),
-#          ('ST2.EXE', ((0xc23b, 0x1085e),),),
-#          ('ST3.EXE', ((0xb49d, 0xee70),),),
-#          ('ST4.EXE', ((0xe263, 0x1620d), (0x1659c, 0x168a8)),),
-#          ('ST5.EXE', ((0xcc01, 0x11465), (0x11977, 0x11b52), (0x11ef2, 0x121fd)),),
-#          ('ST5S1.EXE', ((0x24ee, 0x3af1),),),
-#          ('ST5S2.EXE', ((0x23f9, 0x3797),),),
-#          ('ST5S3.EXE', ((0x3db9, 0x4ed0),),),
-#          ('ST6.EXE', ((0xa51a, 0xcdf4),),),
-#          ('ENDING.EXE', ((0x3c4e, 0x4b1f),),),
-#          ('SINKA.DAT', ((0x0000, 0x874a),),),
-#          ('SEND.DAT', ((0x000, 0x8740),),),
-#          ('46.EXE', ((0x93e8, 0x946d), (0x94b9, 0x971b), (0x9cb8, 0xa07a)))
+files = [ ('OPENING.EXE', ((0x4dda, 0x5868),),),
+('ST1.EXE', ((0xd873, 0xd933), (0xd984, 0x10f85), (0x10fca, 0x11565), (0x11839, 0x119a3), (0x11d42, 0x1204e),),),
+          ('ST2.EXE', ((0xc23b, 0x1085e),),),
+          ('ST3.EXE', ((0xb49d, 0xee70),),),
+          ('ST4.EXE', ((0xe263, 0x1620d), (0x1659c, 0x168a8)),),
+          ('ST5.EXE', ((0xcc01, 0x11465), (0x11977, 0x11b52), (0x11ef2, 0x121fd)),),
+          ('ST5S1.EXE', ((0x24ee, 0x3af1),),),
+          ('ST5S2.EXE', ((0x23f9, 0x3797),),),
+          ('ST5S3.EXE', ((0x3db9, 0x4ed0),),),
+          ('ST6.EXE', ((0xa51a, 0xcdf4),),),
+          ('ENDING.EXE', ((0x3c4e, 0x4b1f),),),
+          ('SINKA.DAT', ((0x0000, 0x874a),),),
+          ('SEND.DAT', ((0x000, 0x8740),),),
+          ('46.EXE', ((0x93e8, 0x946d), (0x94b9, 0x971b), (0x9cb8, 0xa07a)))
 ]
 
 workbook = xlsxwriter.Workbook('shinkaron_dump.xlsx')
@@ -59,6 +61,7 @@ dump_files = []
 
 for file in files:
     for (block_start, block_end) in file[1]:
+        dat_dump = file[0] == 'SINKA.DAT' or file[0] == 'SEND.DAT'
         block_length = block_end - block_start
         in_file = open(file[0], 'rb')
         in_file.seek(block_start)
@@ -66,7 +69,11 @@ for file in files:
         only_hex = ""
         for c in bytes:
             only_hex += "\\x%02x" % ord(c)
-        snippets = only_hex.split('\\x00')
+        if dat_dump:
+            snippets = only_hex.split('\\x0d\\x0a')  # these don't have any x00s in them
+            print snippets
+        else:
+            snippets = only_hex.split('\\x00')
         for snippet in snippets:
             if snippet and len(snippet) > 4:
                 snippet_start = only_hex.index(snippet)
@@ -77,16 +84,18 @@ for file in files:
                 snippet_file.write(snippet_bytes)
                 snippet_file.close()
                 
-                
                 snippet_dump = "dump_" + snippet_filename
-                subprocess.call(".\SJIS_Dump %s %s 1 0" % (snippet_filename, snippet_dump))
+                if dat_dump:
+                    subprocess.call(".\SJIS_Dump %s %s 1 1" % (snippet_filename, snippet_dump))
+                else:
+                    subprocess.call(".\SJIS_Dump %s %s 1 0" % (snippet_filename, snippet_dump))
                 
                 dump_files.append(snippet_dump)
                 os.remove(snippet_filename)
         
 dump = []
-print dump_files
-print len(dump_files)
+#print dump_files
+#print len(dump_files)
         
 for file in dump_files:
     # file.split('_') is ('dump', 'snippet', sourcefile, offset)
@@ -109,7 +118,7 @@ for file in dump_files:
         total_offset += int(offset_within_snippet, 16)
             
         total_offset = hex(total_offset)
-        print total_offset
+        #print total_offset
         
         text = lines[n+1]
     
@@ -125,7 +134,8 @@ for snippet in dump:
     worksheet.write(excel_row, 2, snippet[2])
     excel_row += 1
 
-    
-    #os.remove(file_dump)
-
 workbook.close()
+
+for file in dump_files:
+    #os.remove(file)       #Wonder why it's not finding these?
+    pass
